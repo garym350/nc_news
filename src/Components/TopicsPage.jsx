567008309c7cf42c7ef0eacfcec188d8d4d57236
garym350 <garym350@hotmail.co.uk>
2025-05-24 22:08:51 +0100

    11 CORE sort articles

---
+++ src/Components/TopicsPage.jsx
@@ -0,0 +1,94 @@
+
+import Header from "./Header";
+import ncNewsAPI from "../api";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+const TopicsPage = () => {
+  const [topics, setTopics] = useState([]);
+  const [topic, setTopic] = useState("all")
+  const [loadingTopics, setLoadingTopics] = useState(true);
+  const [loadingTopicsError, setLoadingTopicsError] = useState(false);
+  const [loadingArticles, setLoadingArticles] = useState(true);
+  const [loadingArticlesError, setLoadingArticlesError] = useState(false);
+  
+  
+  const [articles, setArticles] = useState(null)
+
+  useEffect(() => {
+    ncNewsAPI
+      .get("api/topics")
+      .then((response) => {
+        setTopics([...response.data.topics,{"slug":"all"}]);
+        console.log("TOPICS LOADED -->", response.data.topics);
+        setLoadingTopics(false);
+      })
+      .catch((err) => {
+        console.log("TOPICS FAILED TO LOAD");
+        setLoadingTopicsError(true);
+      });
+  }, []);
+
+  ///-------
+
+  useEffect(() => {
+    ncNewsAPI
+      .get("api/articles", { params: {topic}})
+      .then((response) => {
+        setArticles(response.data.articles);
+        console.log("ARTICLES LOADED -->", response.data.articles);
+        setLoadingArticles(false);
+      })
+      .catch((err) => {
+        console.log("ARTICLES FAILED TO LOAD");
+        setLoadingArticlesError(true);
+      });
+  }, [topic]);
+
+  //-------------------
+
+  if (loadingTopics) return <p>Loading Topics...</p>;
+  if (loadingTopicsError) return <p style={{ color: "red" }}>LOADING TOPICS ERROR</p>;
+
+  if (loadingArticles) return <p>Loading Articles...</p>;
+  if (loadingArticlesError) return <p style={{ color: "red" }}>LOADING ARTICLES ERROR</p>;
+
+
+  return (
+    <>
+      <Header />
+      <h2>Topics Page</h2>
+
+      <select
+        onChange={(e) => setTopic(e.target.value)}
+        value={topic}
+      >
+        <option value="" disabled>
+          Select a topic
+        </option>
+        {topics.map((topic) => (
+          <option key={topic.slug} value={topic.slug}>
+            {topic.slug}
+          </option>
+        ))}
+      </select>
+
+      <p style={{fontWeight: "bold"}}>Articles for {topic}</p>
+
+      <Link to={`/topics/${topic}/`}>/topics/{topic}/</Link>
+
+      <div>
+        <ul>
+            {articles.map((article)=>(
+                <li key={ article.article_id }>Title: {article.title}
+                </li>
+            )
+            )
+            }
+        </ul>
+      </div>
+    </>
+  );
+};
+
+export default TopicsPage;
