6625ee7427d29602c4227aae897ae1774466c4d7
garym350 <garym350@hotmail.co.uk>
2025-05-26 17:08:40 +0100

    Code refactored to extract comment count from article object

--- src/Components/ArticleCard.jsx
+++ src/Components/ArticleCard.jsx
@@ -4,60 +4,63 @@ import { useNavigate } from "react-router-dom";
 import useArticleComments from "./useArticleComments";
 import { Link } from "react-router-dom";
 
-const ArticleCard = ({article}) => {
-const navigate=useNavigate();
-const [articleVotes, setArticleVotes] = useState(article.votes);
-const [numComments, setNumComments] = useState(0)
+const ArticleCard = ({ article }) => {
+  const navigate = useNavigate();
+  const [articleVotes, setArticleVotes] = useState(article.votes);
+  const [numComments, setNumComments] = useState(0);
 
-const { errorComments, loadingComments, comments } = useArticleComments;
+  // const { errorComments, loadingComments, comments } = useArticleComments;
 
-useEffect(()=>{
-  if (comments){
-    setNumComments(comments.length)
-  }
-})
+  // useEffect(() => {
+  //   if (comments) {
+  //     setNumComments(comments.length);
+  //   }
+  // });
 
-  if (loadingComments) {
-    return <p>Loading...</p>;
-  }
-  if (errorComments) {
-    return <p style={{ color: "red" }}>ERROR LOADING</p>;
-  }
+  // if (loadingComments) {
+  //   return <p>Loading...</p>;
+  // }
+  // if (errorComments) {
+  //   return <p style={{ color: "red" }}>ERROR LOADING</p>;
+  // }
 
-let votes=articleVotes;
+  let votes = articleVotes;
 
   const handleVotes = () => {
-    votes++
-    setArticleVotes(votes)
+    votes++;
+    setArticleVotes(votes);
     ncNewsAPI
-    .patch(`/api/articles/${article.article_id}`, { inc_votes: 1 })
-    .then((response)=>{
-    })
-    .catch((err)=>{
-    setArticleVotes(votes-1)
-    setError("Voting failed...")
-    })
-  }
+      .patch(`/api/articles/${article.article_id}`, { inc_votes: 1 })
+      .then((response) => {})
+      .catch((err) => {
+        setArticleVotes(votes - 1);
+        setError("Voting failed...");
+      });
+  };
 
   return (
     <Link to={`/article/${article.article_id}`}>
-    <div className="article-card">
-      <h2> {article.title} </h2>
-      <p>Written by {article.author} </p>
-      <img style={{ height: "50px", width: "50px"}}
-        src={article.article_img_url}
-        alt="Article image"
-        className="article-image"
-      />
-      <p></p>
-      <button
-        className="list-group-item list-group-item-action"
-        onClick={() => navigate(`/commentspage/${article.article_id}`)}>
-        {numComments} comments
-      </button>
-      <p></p>
-      <button onClick={handleVotes}>Votes: {articleVotes} - click to vote</button>
-    </div>
+      <div className="article-card">
+        <h2> {article.title} </h2>
+        <p>Written by {article.author} </p>
+        <img
+          style={{ height: "50px", width: "50px" }}
+          src={article.article_img_url}
+          alt="Article image"
+          className="article-image"
+        />
+        <p></p>
+        <button
+          className="list-group-item list-group-item-action"
+          onClick={() => navigate(`/commentspage/${article.article_id}`)}
+        >
+          {article.comment_count} comments
+        </button>
+        <p></p>
+        <button onClick={handleVotes}>
+          Votes: {articleVotes} - click to vote
+        </button>
+      </div>
     </Link>
   );
 };
